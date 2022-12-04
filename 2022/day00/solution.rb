cacea7718a5eccfc47b58e3671664844f9ea012f
Laurent Stacul <laurent.stacul@gmail.com>
2022-12-04 09:04:13 +0100

    Day 4

--- 2022/day00/solution.rb
+++ 2022/day00/solution.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
-def compute(input)
+def part1(input)
+  input
+end
+
+def part2(input)
   input
 end
 
 def main
   input = ARGF.readlines.map(&:chomp)
-  puts "Answer: #{compute(input)}"
+  puts "Answer (part 1): #{part1(input)}"
+  puts "Answer (part 2): #{part2(input)}"
 end
 
 unless ENV.fetch("RUN_TEST", nil) == "1"
@@ -17,7 +22,11 @@ end
 require "minitest/autorun"
 
 class TestSolution < Minitest::Test
-  def test_compute
-    assert_equal 42, compute(0)
+  def test_part1
+    assert_equal 0, compute(0)
+  end
+
+  def test_part2
+    assert_equal 0, compute(0)
   end
 end
---
+++ 2022/day04/solution.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+def parse_line(line)
+  line.split(",").map do |sections|
+    Range.new(*sections.split("-").map(&:to_i))
+  end
+end
+
+def compute(input)
+  overlaps = 0
+  input.each do |line|
+    ranges = parse_line(line)
+    overlaps += 1 if yield(ranges.first.to_a, ranges.last.to_a)
+  end
+
+  overlaps
+end
+
+def part1(input)
+  overlaps = ->(arr1, arr2) { [arr1, arr2].include?(arr1 & arr2) }
+  compute(input, &overlaps)
+end
+
+def part2(input)
+  overlaps_at_all = ->(arr1, arr2) { !(arr1 & arr2).empty? }
+  compute(input, &overlaps_at_all)
+end
+
+def main
+  input = ARGF.readlines.map(&:chomp)
+  puts "Answer (part 1): #{part1(input)}"
+  puts "Answer (part 2): #{part2(input)}"
+end
+
+unless ENV.fetch("RUN_TEST", nil) == "1"
+  main
+  exit
+end
+
+require "minitest/autorun"
+
+class TestSolution < Minitest::Test
+  INPUT = <<~INPUT
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+  INPUT
+
+  def test_part1
+    assert_equal 2, part1(INPUT.split("\n"))
+  end
+
+  def test_part2
+    assert_equal 4, part2(INPUT.split("\n"))
+  end
+
+  def test_real
+    input = File.open("input.txt").readlines.map(&:chomp)
+    assert_equal 471, part1(input)
+    assert_equal 888, part2(input)
+  end
+end
